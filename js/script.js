9ad84b557e818219f58a9541107d2eb827bc80af
Lukasz <lukaszskuzjus@gmail.com>
2018-07-23 12:58:01 +0200

    ...

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 window.onload= function(){
 
     var kolkoKwadrat = document.getElementsByClassName("kolko-kwadrat")[0];
-    var squareInvisibleOverlay = document.getElementsByClassName("squareInvisibleOverlay")[0];
+    var hamburgerMenu = document.getElementsByClassName("hamburger-menu")[0];
+    var squareInvisibleOverlay = document.getElementsByClassName("squareInvisibleOverKolko-kwadrat")[0];
     squareInvisibleOverlay.addEventListener("click",function(){
     kolkoKwadrat.classList.toggle("kolko-kwadrat-rotate");
     });
-
+    hamburgerMenu.addEventListener("click",function(){
+        hamburgerMenu.style.display="block";
+    });
+    
 };
 
